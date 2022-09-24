d846405412e4083c9606c40da7044c90d90dd957
huangliangbo <864181495@qq.com>
2022-09-24 12:48:46 +0800

    添加InputSelect组件

---
+++ src/components/form/widgets/components/InputSelect.vue
@@ -0,0 +1,42 @@
+<script lang="tsx">
+  import { defineComponent, PropType, reactive } from 'vue'
+  export default defineComponent({
+    name: 'InputSelect',
+    inheritAttrs: false,
+    props: {
+      options: {
+        type: Array as PropType<any[]>,
+      },
+    },
+    setup(_props, { attrs }) {
+      return () => (
+        <div class="my--input-select">
+          <a-input {...attrs} />
+          <ul class="select">
+            <li>aaaa</li>
+            <li>aaaa</li>
+            <li>aaaa</li>
+            <li>aaaa</li>
+            <li>aaaa</li>
+          </ul>
+        </div>
+      )
+    },
+  })
+</script>
+
+<style lang="less" scoped>
+  .my--input-select {
+    position: relative;
+
+    .select {
+      position: absolute;
+      top: 100%;
+      left: 0;
+      right: 0;
+      z-index: 999;
+      background: #fff;
+      border: 1px solid #aaa;
+    }
+  }
+</style>
